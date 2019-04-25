377c863da6af6440cf6186ff27b5d0f92f95bd68
Caridad Rivera <ccrive10@gmail.com>
2019-04-25 15:10:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+var dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left;
+dodger.style.bottom;
+dodger.style.bottom = "0px";
+
+// let game = document.getElementById("game");
+// let p = document.createElement("p")
+// p.color = "#000000"
+// p.innerText = "I am here"
+// game.appendChild(p)
+
+// document.addEventListener("keydown", function(e) {
+//   console.log(e.key);
+// });
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10)
+  // console.log(left)
+
+  if (left > 0) {
+    dodger.style.left = `${left-1}px`
+  }
+}
+
+
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
